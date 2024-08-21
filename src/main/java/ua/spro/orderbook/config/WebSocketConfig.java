761aad44f949b20d883a1043252699b60169001d
Oleksandr Prokopenko <oleksandr.prokopenko@student.itmont.de>
2024-08-21 17:02:31 +0300

    refactoring config

--- src/main/java/ua/spro/orderbook/config/WebSocketConfig.java
+++ src/main/java/ua/spro/orderbook/config/WebSocketConfig.java
@@ -20,6 +20,6 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
   @Override
   public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-    registry.addHandler(webSocketHandler, "/binance").setAllowedOrigins("*");
+    registry.addHandler(webSocketHandler, "/binance");
   }
 }
