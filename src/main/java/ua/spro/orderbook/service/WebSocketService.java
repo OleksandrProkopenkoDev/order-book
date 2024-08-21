41c023d3dbd19d42cf7e55af713a5c44025f31a9
Oleksandr Prokopenko <oleksandr.prokopenko@student.itmont.de>
2024-08-21 20:02:26 +0300

    shut down if not connected to WS

--- src/main/java/ua/spro/orderbook/service/WebSocketService.java
+++ src/main/java/ua/spro/orderbook/service/WebSocketService.java
@@ -1,7 +1,11 @@
 package ua.spro.orderbook.service;
 
+import java.util.concurrent.CompletableFuture;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import ua.spro.orderbook.config.WebSocketConfig;
@@ -9,10 +13,10 @@ import ua.spro.orderbook.config.WebSocketConfig;
 @Service
 public class WebSocketService {
 
+  private static final Logger log = LoggerFactory.getLogger(WebSocketService.class);
   private final WebSocketHandlerImpl webSocketHandler;
   private final WebSocketClient webSocketClient;
   private final WebSocketConfig webSocketConfig;
-
 
   @Autowired
   public WebSocketService(WebSocketHandlerImpl webSocketHandler, WebSocketConfig webSocketConfig) {
@@ -23,6 +27,16 @@ public class WebSocketService {
   }
 
   public void connect() {
-    webSocketClient.execute(webSocketHandler, webSocketConfig.getBinanceWsUrl());
+    CompletableFuture<WebSocketSession> sessionFuture =
+        webSocketClient.execute(webSocketHandler, webSocketConfig.getBinanceWsUrl());
+
+    sessionFuture.whenComplete(
+        (session, throwable) -> {
+          if (throwable != null) {
+            log.error("Failed to connect to WebSocket : {}", webSocketConfig.getBinanceWsUrl());
+            // Shut down the application
+            System.exit(1);
+          }
+        });
   }
 }
