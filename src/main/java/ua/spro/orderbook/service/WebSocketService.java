733fa82a80fa6a6b9da3879ffe0cf9cdceba4684
Oleksandr Prokopenko <oleksandr.prokopenko@student.itmont.de>
2024-08-21 16:42:19 +0300

    refactoring WebSocketService

--- src/main/java/ua/spro/orderbook/service/WebSocketService.java
+++ src/main/java/ua/spro/orderbook/service/WebSocketService.java
@@ -1,18 +1,13 @@
 package ua.spro.orderbook.service;
 
-import java.util.concurrent.CompletableFuture;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 
 @Service
 public class WebSocketService {
 
-  private static final Logger log = LoggerFactory.getLogger(WebSocketService.class);
   private final WebSocketHandlerImpl webSocketHandler;
   private final WebSocketClient webSocketClient;
 
@@ -27,17 +22,6 @@ public class WebSocketService {
   }
 
   public void connect() {
-    CompletableFuture<WebSocketSession> sessionFuture =
-        webSocketClient.execute(webSocketHandler, BINANCE_WS_URL);
-
-    sessionFuture.whenComplete(
-        (session, throwable) -> {
-          if (throwable != null) {
-            log.error("Failed to connect to WebSocket", throwable);
-            connect();
-          } else {
-            log.info("WebSocket connection established to Binance.");
-          }
-        });
+    webSocketClient.execute(webSocketHandler, BINANCE_WS_URL);
   }
 }
